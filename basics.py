07dea97e118992d6d8d57791b1f487db376d721e
Nizar <nezarzaid472@gmail.com>
2026-01-04 17:50:23 +0300

    Refactor Student class with GPA encapsulation
    Replaced Starting.py with basics.py, enhancing the Student class to include GPA encapsulation, getter and setter methods with validation. This improves data integrity and adds functionality for managing student GPA.

---
+++ basics.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self,  name, major, gpa) -> None:
+        self.name = name
+        self.major = major
+        self.__gpa = gpa  #Encapsolation
+
+    def set_gpa(self, value): ## self mean  the var in the class
+       ## To set the gpa data with validation
+       if 0 <= value <= 4:
+          self.__gpa = value
+          print("GPA Added")
+       else:
+          print("Enter A number between  0 and 4")
+
+    def get_gpa(self):
+       return self.__gpa
+
+    def greeting (self):
+     print(f"Welcome my name is {self.name} and my major is {self.major}")
+
+
+std1 = Student("Nizar", "Ai and Data", 3.5)
+std1.greeting()
