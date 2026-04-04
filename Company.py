f0861e44ecf9fb975a2e2bce0e82b28126deaadd
Nizar <nezarzaid472@gmail.com>
2026-04-04 13:12:45 +0300

    feat: Builded the full class

--- Company.py
+++ Company.py
@@ -1,24 +1,28 @@
-class Manager:
-    def __init__(self, departmment:str, team_size:int):
-        self.__departmment = departmment
-        self.__team_size = team_size
-    
-    def manage_team(self):
-        pass
-    def set_team_size(self):
-        pass
-
-class Employee(Manager):
-    def __init__(self, departmment, team_size, name:str, employee_id:int, salary:float):
-        super().__init__(departmment, team_size)
+class Employee:
+    def __init__(self,  name:str, employee_id:int, salary:float):
         self.__name = name
         self.__employee_id = employee_id
         self.__salary = salary
 
+    
     def get_details(self):
         pass
 
     def calculate_bouns(self):
+        pass
+
+    
+class Manager(Employee):
+    def __init__(self, departmment, team_size):
+        super().__init__(departmment, team_size)
+        self.__departmment = departmment
+        self.__team_size = team_size
+
+
+    def manage_team(self):
+        print(f"Manager is leading {self.__team_size} people")
+
+    def set_team_size(self):
         pass
 
 e1 = Employee()
